a63d4893fb4ea6ebe8f736b9e19c8dfa46d00481
Yuri Smychkovich <yuri.smychkovich@sophos.com>
2018-10-01 11:16:06 -0400

    Formatting

--- src/main/java/com/sophos/demo/DemoApplication.java
+++ src/main/java/com/sophos/demo/DemoApplication.java
@@ -2,11 +2,12 @@ package com.sophos.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 @SpringBootApplication
 public class DemoApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
 }
